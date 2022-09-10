aa3e0df8b687bfa8abcdd44604b629fce260cb5a
Dmytro <ihnatenko.dev@gmail.com>
2022-09-10 18:09:43 +0300

    Create <Footer/>

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import s from './Footer.module.scss';
+
+import { ReactComponent as Twitter } from '../../assets/icons/social/twitter.svg';
+import { ReactComponent as Facebook } from '../../assets/icons/social/facebook.svg';
+import { ReactComponent as Linkedin } from '../../assets/icons/social/linkedin.svg';
+
+const Footer = () => {
+	return (
+		<footer className={s.footer}>
+			<div className="container">
+				<div className={s.footer__wrapper}>
+					<div className={s.footer__contacts}>
+						<h2>Contacts</h2>
+						<p>2019 Rootz Foundation. All rights reserved</p>
+					</div>
+					<div className={s.footer__headquarters}>
+						<h3>Headquarters</h3>
+						<address>
+							1234 TalibanLos <br /> Angeles, La 1234567 <br />
+							(123) 456-7890
+						</address>
+					</div>
+					<div className={s.footer__social}>
+						<h3>Social media</h3>
+						<div className={s.footer__social_icons}>
+							<Twitter />
+							<Facebook />
+							<Linkedin />
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Intro from '../components/Intro/Intro';
 import Header from '../components/Header/Header';
 import StartedForm from '../components/StartedForm/StartedForm';
 import StartedInfo from '../components/StartedInfo/StartedInfo';
+import Footer from '../components/Footer/Footer';
 
 const Home = () => {
 	return (
@@ -10,6 +11,7 @@ const Home = () => {
 			<Intro />
 			<StartedForm />
 			<StartedInfo />
+			<Footer />
 		</>
 	);
 };
